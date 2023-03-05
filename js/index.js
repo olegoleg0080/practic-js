cb7455887155bec86cc2da3f95f2c3ec9bdfe0e9
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-03-05 15:45:41 +0200

    leason1

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*const browser = "Edge";
+if (browser == 'Edge'){
+    alert("You've got the Edge!");
+} else if(browser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera'){
+    alert('Okay we support these browsers too');
+} else if(browser == null){
+    alert('We hope that this page looks ok!');
+}*/
+let abc = "abc";
+
+if(abc.charAt(0) === "a") {
+    console.log('да');
+}else{
+    console.log("не");
+}
