ca4aade3907866a0e3a50a3451649982536d273d
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-05-27 22:38:55 +0300

    git

---
+++ js/clickerr.js
@@ -0,0 +1,197 @@
+function dqs(selector) {
+    return document.querySelector(selector);
+}
+const refs = {
+    clickerBtn: dqs('.clicker-btn'),
+    createName: dqs('.name-create'),
+    createColor: dqs('.color-input'),
+    basts: dqs('.basts'),
+    btnCreate: dqs('.create'),
+    timer: dqs('.timer'),
+    accountList: dqs('.list-account'),
+    scoreList: dqs('.list-score'),
+    scoreAccount: dqs('.score-account'),
+    place: dqs('.place'),
+    try: dqs('.try-Again'),
+}
+let nameS;
+let nameP;
+let bestS = 0;
+let clicks = 0;
+refs.basts.textContent = localStorage.getItem('Best Score')
+if (!localStorage.getItem('Best Score')) {
+    refs.basts.textContent = 0
+}
+refs.clickerBtn.addEventListener('click', clickerStart);
+refs.try.addEventListener('click', tryAgain);
+function clickerTimer(){
+    let seconds = 9;
+    const timerInterval = setInterval(() => {
+        refs.timer.textContent = seconds;
+        seconds -= 1;
+        if (seconds < 0) {
+            clearInterval(timerInterval);
+            clickerStop();
+            refs.clickerBtn.removeEventListener('click', clickCach)
+            refs.try.style= "display: block;";
+        }
+    }, 1000);
+}
+function clickerStart() {
+    if (!document.querySelector('.select')) {
+        alert('выберите акаунт')
+        return;
+    }
+    refs.clickerBtn.textContent = clicks;
+    refs.clickerBtn.removeEventListener('click', clickerStart);
+    refs.clickerBtn.addEventListener('click', clickCach)
+    clickerTimer()
+}
+function clickCach() {
+    clicks+=1;
+    refs.clickerBtn.textContent = clicks;
+}
+function clickerStop() {
+    if (clicks > localStorage.getItem('Best Score')) {
+        bestS = clicks;
+        localStorage.setItem('Best Score', bestS);
+        refs.basts.textContent = bestS
+    }
+    nameP.textContent = clicks;
+    updateScore(nameS, clicks)
+    sortAccount()
+    clicks = 0;
+}
+function tryAgain() {
+    refs.timer.textContent = 10;
+    refs.clickerBtn.textContent = 'start';
+    refs.clickerBtn.addEventListener('click', clickerStart);
+    refs.try.style= "display: none;";
+}
+populateAccounts();
+function dataSave() {
+    const name = refs.createName.value;
+    const color = refs.createColor.value;
+    let scoreResult = refs.clickerBtn.textContent;
+    if (name === '') {
+        alert('Введите имя');
+        return;
+    }
+    let userList = JSON.parse(localStorage.getItem('userList')) || {};
+
+    if (userList[name]) {
+        alert('такое имя уже существует');
+        return;
+    }
+    if (scoreResult == 'start') {
+        scoreResult = 0;
+    }
+    userList[name] = {
+        name: name,
+        color: color,
+        score: scoreResult,
+    };
+    localStorage.setItem('userList', JSON.stringify(userList));
+    populateAccounts()
+    refs.createName.value = '';
+}
+function createAccount(name, color, scoreResult) {
+    const user = `
+    <div class="account delete-account">
+    <h2 class="name-account">${name}</h2>
+    <div class="color" style="background-color: ${color};"></div>
+    <button class="delete">
+        <svg class="account-svg">
+            <use href="../img/svg/delete-accaunt.svg#Layer_1"></use>
+        </svg>
+    </button>
+    </div>`
+    const scoreUser = `
+    <div class="score-account">
+        <div class="place"></div>
+        <div class="score  ${name}">${scoreResult}</div>
+        <div class="color" style="background-color: ${color};"></div>
+        <div class="name-account name-score">${name}</div>
+    </div>`
+    refs.accountList.insertAdjacentHTML('beforeend', user);
+    refs.scoreList.insertAdjacentHTML('beforeend', scoreUser);
+}
+function populateAccounts() {
+    const userList = JSON.parse(localStorage.getItem('userList'));
+    if (userList) {
+    refs.accountList.innerHTML = '';
+    refs.scoreList.innerHTML = '';
+    const sortedAccounts = Object.values(userList).sort((a, b) => b.score - a.score);
+    sortedAccounts.forEach((account) => {
+        const { name, color, score } = account;
+        createAccount(name, color, score);
+    });
+    }
+}
+function select(accountEl) {
+    const selectedAccount = refs.accountList.querySelector('.select');
+    if (selectedAccount) {
+        selectedAccount.classList.remove('select');
+        tryAgain()
+    }
+    nameS =  accountEl.querySelector('.name-account').textContent;
+    nameP = document.querySelector(`.${nameS}`)
+    console.log(nameP);
+    accountEl.classList.add('select');
+}
+function deleteAccount(event) {
+    const deleteBtn = event.target.closest('.delete');
+    if(!deleteBtn) return;
+
+    const accountEl = deleteBtn.closest('.account')
+    const nameEl = accountEl.querySelector('.name-account')
+    const name = nameEl.textContent;
+    accountEl.remove();
+
+    const userList = JSON.parse(localStorage.getItem('userList'));
+    if(userList && userList[name]){
+        delete userList[name];
+        localStorage.setItem('userList', JSON.stringify(userList));
+    }
+
+    const scoreAccounts = document.querySelectorAll('.score-account');
+    scoreAccounts.forEach((scoreAccount) => {
+        const scoreNameEl = scoreAccount.querySelector('.name-account');
+        if (scoreNameEl.textContent === name) {
+            scoreAccount.remove();
+        }
+    });
+}
+function accountClick(event) {
+    const accountEl = event.target.closest('.account');
+    if (!accountEl) return;
+
+    select(accountEl);
+}
+function updateScore(nameS, newScore) {
+    let userList = JSON.parse(localStorage.getItem('userList')) || {};
+    if (userList[nameS]) {
+    userList[nameS].score = newScore;
+    localStorage.setItem('userList', JSON.stringify(userList));
+    }
+}
+function sortAccount() {
+    const scoreAccounts = Array.from(document.querySelectorAll('.score-account'));
+    scoreAccounts.sort((a, b) => {
+        const scoreA = parseInt(a.querySelector('.score').textContent);
+        const scoreB = parseInt(b.querySelector('.score').textContent);
+        return scoreB - scoreA;
+    });
+    const scoreList = document.querySelector('.list-score');
+    scoreAccounts.forEach((account) => {
+        scoreList.appendChild(account);
+    });
+}
+refs.accountList.addEventListener('click', accountClick);
+refs.btnCreate.addEventListener('click', dataSave);
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        dataSave()
+    }
+});
+refs.accountList.addEventListener('click', deleteAccount);
--- js/timer.js
+++ js/timer.js
@@ -1,43 +1,114 @@
-const refs = {
-    start: document.querySelector('.btn-left'),
-    no: document.querySelector('.btn-right'),
-    timer: document.querySelector('.timer'),
-}
-class Timer{
-    constructor({ onTick }){
-        this.data;
-        this.isActive = false;
-        this.onTick = onTick;
+// const refs = {
+//     start: document.querySelector('.btn-left'),
+//     no: document.querySelector('.btn-right'),
+//     timer: document.querySelector('.timer'),
+// }
+// class Timer{
+//     constructor({ onTick }){
+//         this.data;
+//         this.isActive = false;
+//         this.onTick = onTick;
 
 
-    }
-    init(){
-        const time = this.gettime(0);
-        this.onTick(time);
-    }
-    start(){
-        if(this.isActive){
-            return
-        };
-        this.isActive = true;
-        this.data = Date.now();
-        setInterval(() => {
-            const data = Date.now() - this.data;
-            const timer = this.gettime(data);
-            this.onTick(timer)
-        }, 1000);
-    }
-    gettime(date) {
-        const hours = String(Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2,'0');
-        const minutes =String(Math.floor((date % (1000 * 60 * 60)) / (1000 * 60))).padStart(2,'0');
-        const second = String(Math.floor((date % (1000 * 60)) / 1000)).padStart(2,'0');
-        return `${hours}:${minutes}:${second}`
+//     }
+//     init(){
+//         const time = this.gettime(0);
+//         this.onTick(time);
+//     }
+//     start(){
+//         if(this.isActive){
+//             return
+//         };
+//         this.isActive = true;
+//         this.data = Date.now();
+//         let stopTime =  setInterval(() => {
+//             const data = Date.now() - this.data;
+//             const timer = this.gettime(data);
+//             this.onTick(timer)
+//         }, 1000);
+//         refs.no.addEventListener('click', () => clearTimer(stopTime))
+//         function clearTimer(timer) {
+//             clearInterval(timer);
+//             refs.timer.textContent = '00:00:00';
+//         }
+//     }
+//     gettime(date) {
+//         const hours = String(Math.floor((date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2,'0');
+//         const minutes =String(Math.floor((date % (1000 * 60 * 60)) / (1000 * 60))).padStart(2,'0');
+//         const second = String(Math.floor((date % (1000 * 60)) / 1000)).padStart(2,'0');
+//         return `${hours}:${minutes}:${second}`
+//     }
+// }
+// const timer = new Timer({
+//         onTick: updateTimer,
+// });
+// refs.start.addEventListener('click', timer.start.bind(timer));
+// function updateTimer(time) {
+//     refs.timer.textContent = time;
+// }
+
+
+
+
+
+
+
+// const makeOrder = dish => {
+//     const DELEY  = 2000;
+// }
+// function success (res){
+//     console.log('success');
+//     console.log(res);
+// }
+// function error(er) {
+//     console.log('error');
+//     console.log(er);
+// }
+
+
+//* const horses = [
+//*     'hrusha',
+//*     'arab',
+//*     'globus',
+//*     'oleg',
+//*     'shinka'
+//* ]
+//* console.log('Start');
+//* console.log(`Winer ${1}`);
+//* console.log('End');
+//* function run(horse) {
+//*     return new Promise((resolve, reject) => {
+//*         const time = randomTime(3000, 5000);
+//*         setTimeout(()=>{
+//*             resolve({horse, time})
+//*         },time)
+//*     })
+//* }
+//* //* run(horses[1]).then(res => console.log(res))
+//* const promises = horses.map(horse => run(horse))
+//* //* console.log(promises);
+//* Promise.race(promises).then(({horse, time}) => console.log(`winer ${horse} at ${time}`));
+//* Promise.all(promises).then(res => res.sort((a, b) => a.time - b.time).map(({horse, time}) => console.log(`${horse} at ${time}`)));
+//* function randomTime(min, max) {
+//*     return Math.floor(Math.random() * (max - min + 1) + min)
+//* }
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function executePromises(initialDelay, delayIncrement, numPromises) {
+    let currentDelay = initialDelay;
+    
+    for (let i = 1; i <= numPromises; i++) {
+    console.log(`Проміс ${i} затримка: ${currentDelay}ms`);
+    await delay(currentDelay);
+    currentDelay += delayIncrement;
     }
 }
-const timer = new Timer({
-        onTick: updateTimer,
-});
-refs.start.addEventListener('click', timer.start.bind(timer));
-function updateTimer(time) {
-    refs.timer.textContent = time;
-}
+
+const initialDelay = parseInt((prompt('Введіть початкову затримку:') * 1000));
+const delayIncrement = parseInt((prompt('Введіть крок збільшення затримки:')) * 1000);
+const numPromises = parseInt(prompt('Введіть кількість промісів:'));
+
+executePromises(initialDelay, delayIncrement, numPromises);
